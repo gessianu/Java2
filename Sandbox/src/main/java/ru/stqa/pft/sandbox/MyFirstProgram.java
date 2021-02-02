f6321149f95d3e7bcedbe120a70101f0a92bf206
gessianu <74189819+gessianu@users.noreply.github.com>
2021-02-02 19:13:55 +0400

    Revert "Переменные и значения"
    This reverts commit 2395e91048547756a6fad1474f0596ebdbd8aa83.

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,10 +3,6 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, " +  "world!");
-
-    int l = 8;
-    int s = l * l;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+    System.out.println("Hello, world!");
   }
 }
