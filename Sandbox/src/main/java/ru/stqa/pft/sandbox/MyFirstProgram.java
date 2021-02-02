9f671fe240af2b0877e197cb3c6b589d642566bc
gessianu <74189819+gessianu@users.noreply.github.com>
2021-02-02 19:04:28 +0400

    Revert "Лекция 1.7"
    This reverts commit 5078b75f292775d69dfe1c50b09b89d90f80e46c.

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,8 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgram{
+public class MyFirstProgram {
 
   public static void main(String[] args) {
-<<<<<<< HEAD
     String somebody = "world";
     System.out.println("Hello, " + somebody +  "!");
 
@@ -11,8 +10,5 @@ public class MyFirstProgram{
     double l = 8.0;
     double s = l * l;
     System.out.println("Площадь квадрата со стороной " + l + " = " + s);
-=======
-   System.out.println("Hello, world!");
->>>>>>> parent of 30e91db... Изменение флрматирования в коде программы
   }
 }
