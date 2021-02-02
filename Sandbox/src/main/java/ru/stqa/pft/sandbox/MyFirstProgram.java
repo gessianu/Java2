4774dd5d1fd534b38b68b18ec963652252b2731a
gessianu <74189819+gessianu@users.noreply.github.com>
2021-02-02 19:13:41 +0400

    Revert "Типы переменных и значений"
    This reverts commit 621d09e21b9720f16cf1e9f285bbf2cc6bddaf45.

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,12 +3,10 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    String somebody = "world";
-    System.out.println("Hello, " + somebody +  "!");
+    System.out.println("Hello, " +  "world!");
 
-
-    double l = 8.0;
-    double s = l * l;
+    int l = 8;
+    int s = l * l;
     System.out.println("Площадь квадрата со стороной " + l + " = " + s);
   }
 }
